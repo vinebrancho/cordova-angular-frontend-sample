52578e601a81d5baf29b85d603b32ce77462c235
Vine Brancho <vinebrancho@gmail.com>
2014-05-29 20:45:41 +0900

    Ignore certificate verification for SSL with backend server
     * oauth2-restapi-server has self signed certificate for SSL test.
       this certificate is rejected by security policy of iOS Platform as default.
       iOS platform only allow certificates issued by public CA like VeriSign.
       So, this as iOS app needs to ignore cert verification for oauth2-restapi-server.
       For this, I implement specific delegate function regarding cert check.
    
       <reference>
       * http://www.cocoanetics.com/2009/11/ignoring-certificate-errors-on-nsurlrequest/

---
+++ platforms/ios/MyClient2/Classes/NSURLRequest+IgnoreSSL.h
@@ -0,0 +1,13 @@
+//
+//  NSURLRequest+IgnoreSSL.h
+//  MyClient2
+//
+//  Created by Yunchan Cho on 2014. 5. 29..
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSURLRequest_IgnoreSSL : NSObject
+
+@end
---
+++ platforms/ios/MyClient2/Classes/NSURLRequest+IgnoreSSL.m
@@ -0,0 +1,13 @@
+//
+//  NSURLRequest+IgnoreSSL.m
+//  MyClient2
+//
+//  Created by Yunchan Cho on 2014. 5. 29..
+//
+//
+
+#import "NSURLRequest+IgnoreSSL.h"
+
+@implementation NSURLRequest_IgnoreSSL
+
+@end
